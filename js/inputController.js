3975778b5561289e54206e9ab4ee9f9fb16d86b8
Mack Levine <mlevine@carrot.igenhome.local>
2016-03-10 16:51:23 -0800

    basic ng-repeat table expanding and contracting functionality added

--- js/inputController.js
+++ js/inputController.js
@@ -1,7 +1,6 @@
 angular.module('inputController', [])
   .controller('inputController', function($scope){
-  var columnHeight = 2;
-  $scope.inputColumns = [
+  $scope.inputRows = [
     [
       {value:'header1'}, 
       {value:'header2'}
@@ -13,15 +12,35 @@ angular.module('inputController', [])
   ];
   //initialize columns here.
   $scope.submitHeaders = function(){
+    //invoke the function we have in the service with all of the bound variables.
   }
-  $scope.addColumns = function(){
+  $scope.addColumn = function removeColumn(){
     console.log("adding column...");
-    columns.push
+    //establish a max and min column count.
+    for (var i = 0; i < $scope.inputRows.length; i++){
+      $scope.inputRows[i].push({
+        value: ""
+      });
+    }
   };
-  $scope.addRow = function(){
-    columnHeight++;
+  $scope.removeColumn = function removeColumn(){
+    //establish a max and min column count.
+    for(var i = 0; i < $scope.inputRows.length; i++){
+      $scope.inputRows[i].pop();
+    }
+  }
+  $scope.addRow = function addRow(){
+    var rowToAdd = [];
+    for(var i = 0; i < $scope.inputRows[0].length; i++){
+      rowToAdd.push({
+        value: ""
+      });
+    }
+    $scope.inputRows.push(rowToAdd);
+    //establish a max and min row count.
   };
-  $scope.removeRow = function(){
-    columnHeight--;
+  $scope.removeRow = function removeRow(){
+    $scope.inputRows.pop();
+    //extablish a max and min row count.
   };
 });
