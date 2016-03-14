b234ca32e11cb63331abbecb9b13c3b5529a92e0
Mack Levine <mlevine@carrot.igenhome.local>
2016-03-14 14:12:56 -0700

    started writing tests

--- test/markdownservicetests.js
+++ test/markdownservicetests.js
@@ -2,5 +2,17 @@
 
 var chai = require('chai');
 var expect = chai.expect;
+var dummyInput = require('./dummyinput1.json');
 
 var jsonMarkdownService = require('../server/jsonmarkdownservice');
+
+describe('jsonMarkdownService', function(){
+	xit('should validate ensure that the headers of a given input are all strings', function(){
+
+	});
+	describe('.validateHeaders() method', function(){
+		xit('should find the width of a column at a given index and stash the values it finds in each cell', function(){
+
+		});
+	});
+});
