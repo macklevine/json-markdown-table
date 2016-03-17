dfc9241f1161fdc8cf677aafbcb62a70263a2f27
macklevine <mackplevine@gmail.com>
2016-03-16 19:34:09 -0700

    sent tableString back in the response; now, need to change css/less to monospace for text area

--- client/js/inputController.js
+++ client/js/inputController.js
@@ -10,12 +10,16 @@ angular.module('input.inputController', [])
       {value:'value2'}
     ]
   ];
+  $scope.textAreaVal = "table will show up here...";
   //initialize columns here.
   $scope.submitHeaders = function submitHeaders(){
     //do we need to accept an argument fro outside of the function?
     console.log($scope.inputRows);
     InputService.sendInputData($scope.inputRows)
-      .then(function(tableString){
+      .then(function(response){
+        var tableString = response.data.tableString;
+        console.log(tableString);
+        $scope.textAreaVal = tableString;
         //populate the textArea with the string we get back from the service.
       });
     //invoke the function we have in the service with all of the bound variables.
